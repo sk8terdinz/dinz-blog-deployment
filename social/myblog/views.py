8958eb87fca467959f1858c8f2109454f26b0572
sk8terdinz <128785003+sk8terdinz@users.noreply.github.com>
2023-08-19 20:46:21 +0700

    Add files via upload
    django final deployment

---
+++ social/myblog/views.py
@@ -0,0 +1,252 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Profile, MyUpdate
+from .forms import UpdateForm, SignUpForm, ProfilePicForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def home(request):
+    if request.user.is_authenticated:
+        form = UpdateForm(request.POST or None)
+        if request.method == "POST":
+            if form.is_valid():
+                weep = form.save(commit=False)
+                weep.user = request.user
+                weep.save()
+                messages.success(request, ("Your Update Has Been Posted!"))
+                return redirect('home')
+
+        weeps = MyUpdate.objects.all().order_by("-created_at")
+        return render(request, 'home.html', {"weeps":weeps, "form":form})
+    else:
+        weeps = MyUpdate.objects.all().order_by("-created_at")
+        return render(request, 'home.html', {"weeps":weeps})
+
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles":profiles})
+    else:
+        messages.success(request, ("You Must Be Logged In To View This Page..."))
+        return redirect('home')
+
+def unfollow(request, pk):
+    if request.user.is_authenticated:
+        # Get the profile to unfollow
+        profile = Profile.objects.get(user_id=pk)
+        # Unfollow the user
+        request.user.profile.follows.remove(profile)
+        # Save our profile
+        request.user.profile.save()
+
+        # Return message
+        messages.success(request, (f"You Have Successfully Unfollowed {profile.user.username}"))
+        return redirect(request.META.get("HTTP_REFERER"))  
+    
+    else:
+        messages.success(request, ("You Must Be Logged In To View This Page..."))
+        return redirect('home')    
+
+def follow(request, pk):
+    if request.user.is_authenticated:
+        # Get the profile to unfollow
+        profile = Profile.objects.get(user_id=pk)
+        # Unfollow the user
+        request.user.profile.follows.add(profile)
+        # Save our profile
+        request.user.profile.save()
+
+        # Return message
+        messages.success(request, (f"You Have Successfully Followed {profile.user.username}"))
+        return redirect(request.META.get("HTTP_REFERER"))  
+    
+    else:
+        messages.success(request, ("You Must Be Logged In To View This Page..."))
+        return redirect('home') 
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        weeps = MyUpdate.objects.filter(user_id=pk).order_by("-created_at")
+        # Post Form Logic
+        if request.method == "POST":
+            # Get current user
+            current_user_profile = request.user.profile
+            # Get form data
+            action = request.POST['follow']
+            # Decide to follow or unfollow
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+            # Save the profile
+            current_user_profile.save()
+                
+        return render(request, "profile.html", {"profile":profile, "weeps":weeps})
+    else:
+        messages.success(request, ("You Must Be Logged In To View This Page..."))
+        return redirect('home')
+
+def followers(request, pk):
+    if request.user.is_authenticated:
+        if request.user.id == pk:
+            profiles = Profile.objects.get(user_id=pk)
+            return render(request, 'followers.html', {"profiles":profiles})
+        else:
+            messages.success(request, ("That's Not Your Profile Page..."))
+            return redirect('home')
+    else:
+        messages.success(request, ("You Must Be Logged In To View This Page..."))
+        return redirect('home')
+
+def follows(request, pk):
+    if request.user.is_authenticated:
+        if request.user.id == pk:
+            profiles = Profile.objects.get(user_id=pk)
+            return render(request, 'follows.html', {"profiles":profiles})
+        else:
+            messages.success(request, ("That's Not Your Profile Page..."))
+            return redirect('home')
+    else:
+        messages.success(request, ("You Must Be Logged In To View This Page..."))
+        return redirect('home')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You Have Been Logged In!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error logging in. Try again!"))
+            return redirect('login')
+    else:
+        return render(request, "login.html", {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("There Have Been Logged Out. See You again!"))
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method =="POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #first_name = form.cleaned_data['first_name']
+            #last_name = form.cleaned_data['last_name']
+            #email = form.cleaned_data['email']
+            # Log in user
+            user = authenticate(username=username, password=password)
+            login(request,user)
+            messages.success(request, ("You have successfully registered! Welcome!"))
+            return redirect('home')
+        
+    return render(request, "register.html", {'form':form})
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        profile_user = Profile.objects.get(user__id=request.user.id)
+        # Get Forms
+        user_form = SignUpForm(request.POST or None, request.FILES or None, instance=current_user)
+        profile_form = ProfilePicForm(request.POST or None, request.FILES or None, instance=profile_user)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            login(request, current_user)
+            messages.success(request, ("Your Profile Has Been Updated!"))
+            return redirect('home')
+        
+        return render(request, "update_user.html", {'user_form':user_form, 'profile_form':profile_form})
+    else:
+        messages.success(request, ("You Must Be Logged In to View That Page!"))
+        return redirect('home')
+
+def update_like(request, pk):
+    if request.user.is_authenticated:
+        weep = get_object_or_404(MyUpdate, id=pk)
+        if weep.likes.filter(id=request.user.id):
+            weep.likes.remove(request.user)
+        else:
+            weep.likes.add(request.user)
+        return redirect(request.META.get("HTTP_REFERER"))
+    
+    else:
+        messages.success(request, ("You Must Be Logged In to View That Page!"))
+        return redirect('home')
+
+def delete_update(request, pk):
+    if request.user.is_authenticated:
+        weep = get_object_or_404(MyUpdate, id=pk)
+        # Check to see if you own the update
+        if request.user.username == weep.user.username:
+            # Delete The Update
+            weep.delete()
+
+            messages.success(request, ("The Update Has Been Deleted!"))
+            return redirect(request.META.get("HTTP_REFERER"))
+        else:
+            messages.success(request, ("You Don't Own That Update!!"))
+            return redirect('home')
+    else:
+        messages.success(request, ("Please Log In To Continue..."))
+        return redirect(request.META.get("HTTP_REFERER"))
+    
+def edit_update(request, pk):
+    if request.user.is_authenticated:
+        # Grab The Update!
+        weep = get_object_or_404(MyUpdate, id=pk)
+        
+        # Check to see if you own the update
+        if request.user.username == weep.user.username:
+            
+            form = UpdateForm(request.POST or None, instance=weep)
+            if request.method == "POST":
+                if form.is_valid():
+                    weep = form.save(commit=False)
+                    weep.user = request.user
+                    weep.save()
+                    messages.success(request, ("Your Update Has Been Updated!"))
+                    return redirect('home')
+            else:
+                return render(request, 'edit_update.html', {'form':form, 'weep':weep})
+
+        else:
+            messages.success(request, ("You Don't Own That Update!!"))
+            return redirect('home')
+    else:
+        messages.success(request, ("Please Log In To Continue..."))
+        return redirect('home')
+
+def search(request):
+    if request.method == "POST":
+        # Grab the form field input
+        search = request.POST['search']
+        # Search the database
+        searched = MyUpdate.objects.filter(body__contains = search)
+        return render(request, 'search.html', {'search':search, 'searched':searched})
+    else:
+        return render(request, 'search.html', {})
+    
+def search_user(request):
+    if request.method == "POST":
+        # Grab the form field input
+        search = request.POST['search']
+        # Search the database
+        searched = User.objects.filter(username__contains = search)
+        return render(request, 'search_user.html', {'search':search, 'searched':searched})
+    else:
+        return render(request, 'search_user.html', {})
